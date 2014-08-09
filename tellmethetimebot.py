75dc87bb57937b125ead2e1a59494610d1783cf8
Hanh Kieu <hckieu@ucdavis.edu>
2014-08-08 18:00:50 -0700

    parsed html with bing

--- tellmethetimebot.py
+++ tellmethetimebot.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3.4
-
-
+import requests
+from bs4 import BeautifulSoup
 def url_generator(user_input):
     words = user_input.split(" ")
-    website_url = "https://www.google.com/#q="
+    website_url = "http://www.bing.com/search?q="
     count = 0
     for word in words:
         if(count == (len(words) - 1) ):
@@ -18,11 +18,11 @@ def url_generator(user_input):
 
 def main():
 
+
+    file_object = open("html.txt","w")
     user_input = "what time is it in china"
     website_url = url_generator(user_input)
-
-    print(website_url)
-
-
+    r = requests.get(website_url)
+    soup = BeautifulSoup(r.content)
+    file_object.write(soup.prettify())
 main()
-
